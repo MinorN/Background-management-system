1e5d0152c06e603c737d7aff7f51d3cb3bcdbe03
qijiajun <1481700559@qq.com>
2022-09-25 22:38:54 +0800

    set token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,32 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user.js'
+
+const state = {
+  token: getToken()
+}
+const mutations = {
+  SET_TOKEN(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  REMOVE_TOKEN(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async LOGIN(context, data) {
+    const result = await login(data)
+    if (result.data.success) {
+      const token = result.data.data
+      context.commit('SET_TOKEN', token)
+    }
+  }
+}
+
 export default {
   namespace: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
